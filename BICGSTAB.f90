d05696404930e7c470887707b6765045ccfcce88
Arman Dindar Safa <armandindar6@gmail.com>
2024-12-15 23:19:37 +0330

    Update BICGSTAB.f90

--- BICGSTAB.f90
+++ BICGSTAB.f90
@@ -1,4 +1,27 @@
-MODULE BICGSTABMOD
+! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!   ARMAN DINDAR SAFA
+!   12/ 15 / 2024
+! 
+! 
+! THIS PROGRAM SOLVES FOR AX = B USING BICONDUGATE STABILITZED METHOD
+! 
+! INPUT FILE FORMAT IN ORDER: 
+! 1. ROWS AND COLS OF MATRIX A
+! 2. A MATRIX 
+! 3. B MATRIX
+! 4. ANALYTICAL SOLUTION IF AVAILABLE ( OPTIONAL )
+! --------------
+! EXAMPLE INPUT:
+! 4 4                    ! 4 x 4 A
+! 1.0 1.0 1.0 1.0        !       A
+! 2.0 3.0 1.0 1.0
+! 3.0 4.0 1.0 1.0
+! 3.0 4.0 1.0 2.0
+! 50.0 96.0 135.0 143.0  ! B
+! 5.0 8.0 10.0 12.0      ! ANALYTICAL
+! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+MODULE BICGSTAB
     IMPLICIT NONE
 
     INTEGER (KIND=2), PARAMETER    :: SP = KIND(1.000)
@@ -20,13 +43,13 @@ CONTAINS
         REAL (KIND=WP)                                :: NORM_R, NORM_B       
         INTEGER (KIND=SP)                             :: IT=0
 
-        IF (SIZE(A, DIM=1) /= SIZE(A, DIM=2)) STOP "ERROR: IMPROPER DIMENSION OF MATRIX A IN BICGSTAB."
+        IF (SIZE(A, DIM=1) /= SIZE(A, DIM=2)) STOP "ERROR: IMPROPER DIMENSIONS FOR MATRIX A IN BICGSTAB."
 
-        X  = 0.0_WP
-        R  = B - MATMUL(A, X)
-        RS = R
-        RHO   = 1.0_WP; ALPHA = 1.0_WP; OMEGA = 1.0_WP
-        V  = 0.0_WP; P  = 0.0_WP
+        X      = 0.0_WP
+        R      = B - MATMUL(A, X)
+        RS     = R
+        RHO    = 1.0_WP; ALPHA = 1.0_WP; OMEGA = 1.0_WP
+        V      = 0.0_WP; P  = 0.0_WP
         NORM_R = SQRT(DOT_PRODUCT(R, R))
         NORM_B = SQRT(DOT_PRODUCT(B, B))
 
@@ -44,42 +67,52 @@ CONTAINS
             R        = S - OMEGA*T
             NORM_R   = SQRT(DOT_PRODUCT(R, R))
             NORM_B   = SQRT(DOT_PRODUCT(B, B))
-            IT = IT + 1
+            IT       = IT + 1
         END DO
 
-        WRITE(*,*) "ITERATIONS REQUIRED :", IT
+        WRITE(*,*) "REQ. ITERATIONS: ", IT
 
         RETURN
     END FUNCTION BICGSTAB     
-END MODULE BICGSTABMOD
+END MODULE BICGSTAB
 
 PROGRAM MAIN
     USE BICGSTABMOD
     IMPLICIT NONE
 
-    INTEGER (KIND=SP), PARAMETER             :: M=4, N=4
-    REAL    (KIND=WP), DIMENSION(1:M,1:N)    :: A
-    REAL    (KIND=WP), DIMENSION(1:M    )    :: X_CALCULATED, X_ACTUAL, B
-    INTEGER                                   :: FILE_UNIT
+    INTEGER (KIND=SP), PARAMETER             :: FILE_UNIT_BCG = 10 
+    INTEGER (KIND=SP), PARAMETER             :: FILE_UNIT_IN  = 20
+    INTEGER (KIND=SP), PARAMETER             :: FILE_UNIT_ERR = 30
+    REAL    (KIND=WP), ALLOCATABLE           :: A(:,:), B(:), X_CALCULATED(:), X_ACTUAL(:)
+    INTEGER                                  :: M, N, I
 
-    A(1,:) = [ 1.0_WP, 1.0_WP, 1.0_WP, 1.0_WP]
-    A(2,:) = [ 2.0_WP, 3.0_WP, 1.0_WP, 1.0_WP]
-    A(3,:) = [ 3.0_WP, 4.0_WP, 1.0_WP, 1.0_WP]
-    A(4,:) = [ 3.0_WP, 4.0_WP, 1.0_WP, 2.0_WP]
+    ! Open the input file
+    OPEN (UNIT=FILE_UNIT_IN, FILE="BICGSTAB_INPUT.TXT", STATUS="OLD", ACTION="READ")
+    READ (FILE_UNIT_IN,*) M, N
+    ALLOCATE (A(1:M,1:N), B(1:M), X_CALCULATED(1:M), X_ACTUAL(1:M))
+    DO I = 1, M
+        READ (FILE_UNIT_IN,*) A(I, :)
+    END DO
+    READ  (FILE_UNIT_IN,*) B(:)
+    READ  (FILE_UNIT_IN,*) X_ACTUAL(:)
+    CLOSE (FILE_UNIT_IN)
 
-    X_ACTUAL(:) = [ 5.0_WP, 8.0_WP,10.0_WP,12.0_WP]
-    B = MATMUL(A, X_ACTUAL)
-
+    ! Solve the system using BiCGStab
     X_CALCULATED = BICGSTAB(A, B)
 
-    FILE_UNIT = 10
-    OPEN (UNIT=FILE_UNIT, FILE="BICGSTAB_RESULTS.TXT", STATUS="REPLACE", ACTION="WRITE")
-    WRITE(FILE_UNIT,*) "ANALYTICAL =", X_ACTUAL
-    WRITE(FILE_UNIT,*) "BICGSTAB   =", X_CALCULATED
-    WRITE(FILE_UNIT,*) "ERROR      =", ABS(X_ACTUAL - X_CALCULATED)/X_ACTUAL
-    CLOSE(FILE_UNIT  )
+    ! Write BiCGStab solution to output file
+    OPEN  (UNIT=FILE_UNIT_BCG, FILE="BICGSTAB_SOLUTION.TXT", STATUS="REPLACE", ACTION="WRITE")
+    WRITE (FILE_UNIT_BCG,*) X_CALCULATED
+    CLOSE (FILE_UNIT_BCG)
+
+    ! Write error to separate output file
+    OPEN  (UNIT=FILE_UNIT_ERR, FILE="BICGSTAB_ERROR.TXT", STATUS="REPLACE", ACTION="WRITE")
+    WRITE (FILE_UNIT_ERR,*) ABS(X_ACTUAL - X_CALCULATED)/X_ACTUAL
+    CLOSE (FILE_UNIT_ERR)
 
     WRITE(*,*) "//"
-    WRITE(*,*) "RESULTS HAVE BEEN WRITTEN TO 'BICGSTAB_RESULTS.TXT'."
+    WRITE(*,*) "RESULTS HAVE BEEN WRITTEN TO 'BICGSTAB_SOLUTION.TXT' AND 'BICGSTAB_ERROR.TXT'."
 
+    ! Deallocate arrays
+    DEALLOCATE (A, B, X_CALCULATED, X_ACTUAL)
 END PROGRAM MAIN
